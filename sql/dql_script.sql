f43558c1a5747027cfc942d8059b392d0cf81a6a
rafadias008 <123601507+rafadias008@users.noreply.github.com>
2025-04-16 02:06:11 +0000

    modificações no dql com novas querys

--- sql/dql_script.sql
+++ sql/dql_script.sql
@@ -1,288 +1,131 @@
 -- histórico escolar de um aluno que teve reprovação em uma disciplina, retornando inclusive a reprovação em um semestre e a aprovação no semestre seguinte
-SELECT 
-    ha.id_aluno,
-    a.nome AS nome_aluno,
-	d.id_disciplina,
-    d.nome AS nome_disciplina,
-    ha.semestre,
-    ha.nota,
-    ha.situacao
-
-FROM Historico_Aluno ha
-
-LEFT JOIN Aluno a ON ha.id_aluno = a.id_aluno
-LEFT JOIN Disciplina d ON ha.id_disciplina = d.id_disciplina
-
-WHERE ha.id_aluno IN (
-	SELECT distinct r.id_aluno FROM Historico_Aluno r
-    LEFT JOIN Historico_Aluno a ON r.id_aluno = a.id_aluno AND r.id_disciplina = a.id_disciplina
-    WHERE r.situacao = 'reprovado' AND a.situacao = 'aprovado' AND a.semestre > r.semestre
-	LIMIT 1
-)
-
-ORDER BY ha.id_aluno, d.id_disciplina, ha.semestre;
+SELECT * 
+FROM historico_aluno 
+WHERE id_aluno = 55 -- pode substituir por outro id_aluno para testar
+ORDER BY id_aluno, id_disciplina, semestre 
 
 
 -- todos os TCCs orientados por um professor junto com os nomes dos alunos que fizeram o projeto
-SELECT 
-	tcc.id_tcc, 
-	tcc.titulo, 
-	tcc.id_orientador, 
-	p.nome as nome_prof, 
-	a.id_aluno, 
-	a.nome as nome_aluno 
+WITH tccs_prof AS (
+	SELECT id_orientador, COUNT(DISTINCT id_tcc) 
+	FROM tcc 
+	GROUP BY id_orientador 
+	ORDER BY COUNT(DISTINCT id_tcc) DESC 
+	LIMIT 1
+)
 
+SELECT tcc.id_tcc, tcc.titulo, tcc.id_orientador, p.nome as nome_prof, a.id_aluno, a.nome as nome_aluno 
 FROM tcc 
-
+INNER JOIN tccs_prof ON tcc.id_orientador = tccs_prof.id_orientador
 LEFT JOIN professor p ON tcc.id_orientador = p.id_professor
 LEFT JOIN aluno_tcc atcc ON tcc.id_tcc = atcc.id_tcc
 LEFT JOIN aluno a ON atcc.id_aluno = a.id_aluno
 
-WHERE tcc.id_orientador IN (SELECT DISTINCT id_orientador FROM tcc LIMIT 1)
-
-ORDER BY tcc.id_orientador, tcc.id_tcc
-
 
 -- matriz curicular de pelo menos 2 cursos diferentes que possuem disciplinas em comum
+WITH disc_curso AS (
+	SELECT DISTINCT id_disciplina, COUNT(DISTINCT id_curso)
+	FROM grade_curso
+	GROUP BY id_disciplina
+	ORDER BY COUNT(DISTINCT id_curso) DESC
+	LIMIT 1
+),
 
--- Ciencia da computação
-SELECT 
-    c.nome AS nome_curso,
-    d.nome AS nome_disciplina,
-    d.codigo
+cur_dist as (
+	SELECT DISTINCT id_curso FROM grade_curso gc
+	INNER JOIN disc_curso dc ON gc.id_disciplina = dc.id_disciplina
+)
 
-FROM grade_curso gc
-
-LEFT JOIN curso c ON gc.id_curso = c.id_curso
+SELECT c.id_curso, c.nome as nom_curso, d.id_disciplina, d.nome as nom_disciplina 
+FROM curso c
+INNER JOIN cur_dist cd ON c.id_curso = cd.id_curso
+LEFT JOIN grade_curso gc ON c.id_curso = gc.id_curso
 LEFT JOIN disciplina d ON gc.id_disciplina = d.id_disciplina
-
-WHERE c.nome = 'Ciencia da Computacao' AND gc.id_disciplina IN (
-      SELECT id_disciplina
-      FROM Grade_Curso
-      WHERE id_curso IN (SELECT id_curso FROM Curso WHERE nome IN ('Ciencia da Computacao', 'Engenharia Civil'))
-      GROUP BY id_disciplina
-      HAVING COUNT(DISTINCT id_curso) >= 2
-  )
-  
-ORDER BY d.nome;
-
--- Engenharia Civil
-SELECT 
-    c.nome AS nome_curso,
-    d.nome AS nome_disciplina,
-    d.codigo
-
-FROM grade_curso gc
-
-LEFT JOIN curso c ON gc.id_curso = c.id_curso
-LEFT JOIN disciplina d ON gc.id_disciplina = d.id_disciplina
-
-WHERE c.nome = 'Engenharia Civil' AND gc.id_disciplina IN (
-      SELECT id_disciplina
-      FROM Grade_Curso
-      WHERE id_curso IN (SELECT id_curso FROM Curso WHERE nome IN ('Ciencia da Computacao', 'Engenharia Civil'))
-      GROUP BY id_disciplina
-      HAVING COUNT(DISTINCT id_curso) >= 2
-  )
-  
-ORDER BY d.nome;
-
+LEFT JOIN disc_curso dc ON d.id_disciplina = dc.id_disciplina
+ORDER BY c.id_curso, d.id_disciplina
 
 -- Para um determinado aluno, mostre os códigos e nomes das diciplinas já cursadas junto com os nomes dos professores que lecionaram a disciplina para o aluno
-SELECT 
-	ha.id_aluno, 
-	ha.semestre, 
-	d.id_disciplina, 
-	d.codigo, 
-	d.nome, 
-	p.id_professor, 
-	p.nome as nom_prof 
-
+SELECT ha.id_aluno, ha.semestre, d.id_disciplina, d.codigo, d.nome, p.id_professor, p.nome as nom_prof 
 FROM historico_aluno ha 
-
 LEFT JOIN disciplina d ON ha.id_disciplina = d.id_disciplina
 LEFT JOIN professor p ON ha.id_professor = p.id_professor
-
 WHERE id_aluno = 12 -- pode substituir por outro id_aluno para testar
-
 ORDER BY id_aluno, ha.id_disciplina, semestre 
 
 
 -- Liste todos os chefes de departamento e coordenadores de curso
-SELECT 
-	p.nome as nom_prof, 
-	COALESCE(d.nome, 'nenhum') as chefe_dpto, 
-	COALESCE(c.nome, 'nenhum') as coord_curso 
-
+SELECT p.nome as nom_prof, COALESCE(d.nome, 'nenhum') as chefe_dpto, COALESCE(c.nome, 'nenhum') as coord_curso 
 FROM professor p
-
 LEFT JOIN departamento d ON p.id_professor = d.id_chefe_departamento
 LEFT JOIN curso c ON p.id_professor = c.id_coordenador
 
 
--- Encontre os nomes dos estudantes que cursaram matérias em mais de 3 departamentos.
-SELECT 
-    a.nome AS nome_aluno
-	,count(distinct d.id_departamento) as total_dpto
+--Encontre os nomes de todos os estudantes.
+SELECT nome
+FROM Aluno;
 
-FROM aluno a
+--Liste os IDs e nomes de todos os professores.
+SELECT id_professor, nome
+FROM Professor;
 
-LEFT JOIN historico_aluno ha ON a.id_aluno = ha.id_aluno
-LEFT JOIN disciplina d ON ha.id_disciplina = d.id_disciplina
-
-GROUP BY a.nome
-
-HAVING COUNT(DISTINCT d.id_departamento) >= 3;
-
-
--- Encontre o número total de estudantes que cursaram "Engenharia Civil"
-SELECT 
-	COUNT(*) AS total_estudantes
-
-FROM aluno a
-
-LEFT JOIN curso c ON a.id_curso = c.id_curso
-
+--Encontre os nomes de todos os estudantes que cursaram "Engenharia Civil"
+SELECT a.nome AS NomeEstudante,
+       c.nome AS NomeCurso
+FROM Aluno a
+JOIN Curso c ON a.id_curso = c.id_curso
 WHERE c.nome = 'Engenharia Civil';
 
+-- Encontre o número total de estudantes que cursaram "Engenharia Civil"
+SELECT COUNT(*) AS total_estudantes
+FROM Aluno a
+JOIN Curso c ON a.id_curso = c.id_curso
+WHERE c.nome = 'Engenharia Civil';
 
 --Liste os professores que ministraram cursos com mais de 20 alunos matriculados
-SELECT DISTINCT 
-    p.id_professor,
-    p.nome AS nome_professor
-
-FROM historico_aluno ha
-
-LEFT JOIN aluno a ON ha.id_aluno = a.id_aluno
-LEFT JOIN curso c ON a.id_curso = c.id_curso
-LEFT JOIN professor p ON ha.id_professor = p.id_professor
-
-WHERE a.id_curso IN (
-        SELECT id_curso
-        FROM Aluno
-        GROUP BY id_curso
-        HAVING COUNT(*) > 20
-);
-
-
--- Liste os nomes dos estudantes que não cursaram nenhuma disciplina no departamento de "Matematica".
-SELECT 
-    a.id_aluno,
-    a.nome AS nome_aluno
-
-FROM aluno a
-
-WHERE NOT EXISTS (
-        SELECT 1
-        FROM historico_aluno ha
-        LEFT JOIN disciplina d ON ha.id_disciplina = d.id_disciplina
-        LEFT JOIN departamento dep ON d.id_departamento = dep.id_departamento
-        WHERE ha.id_aluno = a.id_aluno AND dep.nome = 'Matematica'
-);
-
-
--- Liste as disciplinas que foram ministradas por mais de um professor em semestres diferentes.
-SELECT 
-    d.nome AS nome_disciplina,
-    COUNT(DISTINCT hp.id_professor) AS num_professores,
-    COUNT(DISTINCT hp.semestre) AS num_semestres
-
-FROM historico_Professor hp
-
-LEFT JOIN disciplina d ON hp.id_disciplina = d.id_disciplina
-
-GROUP BY d.id_disciplina
-
-HAVING COUNT(DISTINCT hp.id_professor) > 1 AND COUNT(DISTINCT hp.semestre) > 1;
-
-
--- Liste a média das notas de aprovação por disciplina
-SELECT 
-    d.id_disciplina,
-    d.nome AS nome_disciplina,
-    AVG(ha.nota) AS media_aprovacao
-
-FROM historico_aluno ha
-
-LEFT JOIN disciplina d ON ha.id_disciplina = d.id_disciplina
-
-WHERE ha.situacao = 'aprovado'
-
-GROUP BY d.id_disciplina, d.nome
-
-ORDER BY media_aprovacao DESC;
-
-
--- liste os alunos e a quantidade de vezes que cursaram cada uma das disciplinas 
-SELECT 
-    a.id_aluno,
-    a.nome AS nome_aluno,
-    d.id_disciplina,
-    d.nome AS nome_disciplina,
-    COUNT(*) AS vezes_cursada
-
-FROM historico_aluno ha
-
-LEFT JOIN aluno a ON ha.id_aluno = a.id_aluno
-LEFT JOIN disciplina d ON ha.id_disciplina = d.id_disciplina
-
-GROUP BY a.id_aluno, a.nome, d.id_disciplina, d.nome
-
-ORDER BY a.id_aluno, d.id_disciplina;
-
-
--- Encontre o número de alunos matriculados em cada curso e liste-os por título de curso
-SELECT 
-    c.nome AS nome_curso,
-    COUNT(a.id_aluno) AS total_alunos
-
-FROM curso c
-
-LEFT JOIN aluno a ON a.id_curso = c.id_curso
-
-GROUP BY c.id_curso, c.nome
-
-ORDER BY c.nome;
-
-
--- Liste os 10 alunos com maior desempenho e seus respectivos cursos e media de aprovação
-SELECT 
-    a.id_aluno,
-    a.nome AS nome_aluno,
-    c.nome AS nome_curso,
-    ROUND(AVG(ha.nota), 2) AS media_aprovacao
-
-FROM aluno a
-
-LEFT JOIN curso c ON a.id_curso = c.id_curso
-LEFT JOIN historico_aluno ha ON a.id_aluno = ha.id_aluno
-
-WHERE ha.situacao = 'aprovado'
-
-GROUP BY a.id_aluno, a.nome, c.nome
-
-ORDER BY media_aprovacao DESC
-
-LIMIT 10;
-
-
--- liste os 10 professores com alunos com maior indice de aprovação
-SELECT 
-    p.id_professor,
-    p.nome AS nome_professor,
-    COUNT(CASE WHEN ha.situacao = 'aprovado' THEN 1 END) * 1.0 / COUNT(*) AS indice_aprovacao,
-    COUNT(*) AS total_alunos
-
-FROM historico_aluno ha
-
-LEFT JOIN professor p ON ha.id_professor = p.id_professor
-
-WHERE ha.id_professor IS NOT NULL
-
+SELECT p.id_professor, p.nome
+FROM Professor p
+JOIN Curso c ON p.id_professor = c.id_coordenador
+JOIN Aluno a ON c.id_curso = a.id_curso
 GROUP BY p.id_professor, p.nome
+HAVING COUNT(a.id_aluno) > 20;
 
-HAVING COUNT(*) > 0
+--encontre os nomes dos professores que ministraram cursos nos quais todos os alunos receberam nota '10'.
+SELECT DISTINCT p.nome
+FROM Professor p
+JOIN (
+    SELECT ha.id_professor, ha.id_disciplina, ha.semestre
+    FROM Historico_Aluno ha
+    GROUP BY ha.id_professor, ha.id_disciplina, ha.semestre
+    HAVING MIN(ha.nota) = 10 AND MAX(ha.nota) = 10
+) AS cursos_com_10
+ON p.id_professor = cursos_com_10.id_professor;
 
-ORDER BY indice_aprovacao DESC
 
-LIMIT 10;
+
+--Encontre os estudantes que estão matriculados em cursos oferecidos pelo departamento de "Tecnologia da Informação".
+
+SELECT 
+	aluno.nome
+FROM aluno 
+LEFT JOIN curso ON aluno.id_curso = curso.id_curso
+LEFT JOIN departamento ON departamento.id_departamento = curso.id_departamento
+WHERE departamento.nome = 'Tecnologia da Informacao'
+
+
+--Recupere os IDs dos estudantes que não estão matriculados em nenhum curso do departamento de "Tecnologia da Informação".
+
+SELECT 
+	aluno.id_aluno
+FROM aluno
+LEFT JOIN curso ON aluno.id_curso = curso.id_curso
+LEFT JOIN departamento ON departamento.id_departamento = curso.id_departamento
+WHERE departamento.nome NOT IN ('Tecnologia da Informacao')
+
+--Encontre o número de alunos matriculados em cada curso e liste-os por título de curso.
+
+SELECT 
+	curso.nome,
+	COUNT(id_aluno)
+FROM aluno
+LEFT JOIN curso ON aluno.id_curso = curso.id_curso
+GROUP BY curso.nome
